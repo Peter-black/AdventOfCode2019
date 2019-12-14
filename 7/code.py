f1db068ce861f359db0c688259b56275d2a13ab9
Peter Black <peter.a.black@live.co.uk>
2019-12-14 20:18:37 +0100

    Make Opcode computer a class
    Entirely refactored computer to make it a class with instances, allowed delayed input buffers, program halts and needs restarted. Made adding new ops easier

--- 7/code.py
+++ 7/code.py
@@ -6,137 +6,170 @@ def load_program(filename):
 	print("[INFO] Successfully loaded program [ "+filename+" ]")
 	return opcode_array
 
-def opcode_1(input1, input2):
-	return int(input1) + int(input2)
 
-def opcode_2(input1, input2):
-	return int(input1) * int(input2)
+class Computer:
+	def __init__(self):
+		self.OP_LIST = { 	1 	: (self._opcode_1 , 3),
+							2 	: (self._opcode_2 , 3),
+							3 	: (self._opcode_3 , 1),
+							4 	: (self._opcode_4 , 1),
+							5 	: (self._opcode_5 , 2),
+							6 	: (self._opcode_6 , 2),
+							7	: (self._opcode_7 , 3),
+							8	: (self._opcode_8 , 3),
+							99 	: (self._opcode_99 , 0)	}
 
-def opcode_3():
-	return input("[INPUT] Enter a value: ")
+		self.output_buffer = []
+		self.input_buffer = []
 
-def opcode_4(input1):
-	print("[OUT]: "+str(input1))
+		self.opcode_pointer = 0
+		self.do_jump = False
 
-def opcode_5(input1):
-	return (int(input1) > 0)
+		self.user_input = False
+		self.awaiting_input = False
+		#Used to end the program by opcode 99
+		self.terminate = False
 
-def opcode_6(input1):
-	return (int(input1) == 0)
+	def set_user_input_mode(self, use_user_input):
+		self.user_input = use_user_input
 
-def opcode_7(input1, input2):
-	return int(int(input1) < int(input2))
+	def input(self, input_value):
+		self.input_buffer.append(input_value)
+		self.awaiting_input = False
 
-def opcode_8(input1, input2):
-	return int(int(input1) == int(input2))
+	def load_opcode_program(self, program_data):
+		self.__init__()
+		self.program = list(program_data)
 
-def opcode_99():
-	pass
+	def run_opcode_program(self):
+		
+		while not self.terminate and not self.awaiting_input:
+			self._update()
 
-def run_opcode_program(opcode_array, input_buffer = []):
-	OP_PARAM_COUNT = { 	1 	: 3,
-						2 	: 3,
-						3 	: 1,
-						4 	: 1,
-						5 	: 2,
-						6 	: 2,
-						7	: 3,
-						8	: 3,
-						99 	: 0	}
+		return self.output_buffer
 
-	output_buffer = []
-	opcode_pointer = 0
-	while True:
-		current_instruction = opcode_array[opcode_pointer]
+	###########################################
 
-		step_distance = 0
-		param_mode = [0,0,0]
-		params = [0,0,0]
+	#Location equals sum of inputs
+	def _opcode_1(self):
+		self.program[ self.params[2] ] = self.program[ self.params[0] ] + self.program[ self.params[1] ] 
+	
+	#Location equals multiplication of inputs
+	def _opcode_2(self):
+		self.program[ self.params[2] ] = self.program[ self.params[0] ] * self.program[ self.params[1] ] 
+	
+	#Location equals a program input
+	def _opcode_3(self):
+		input_val = None
+
+		if self.user_input:
+			input_val = input("[INPUT] Enter a value: ")
+		else:
+			if len(self.input_buffer) > 0:
+				input_val = self.input_buffer.pop(0)
+			else:
+				self.awaiting_input = True
+
+		self.program[ self.params[0] ] = input_val
+	
+	#Output a location
+	def _opcode_4(self):
+		output_val = self.program[ self.params[0] ]
+		print("[OUT]: " + str( output_val ))
+		self.output_buffer.append(output_val)
+		
+	#If location is more than 0, jump to location
+	def _opcode_5(self):
+		if self.program[ self.params[0] ] > 0:
+			self.opcode_pointer = self.program[ self.params[1] ]
+			self.do_jump = True
+	
+	#If location equals 0, jump to location
+	def _opcode_6(self):
+		if self.program[ self.params[0] ] == 0:
+			self.opcode_pointer = self.program[ self.params[1] ]
+			self.do_jump = True
+
+	#If input is less than input, set location to 1 or 0
+	def _opcode_7(self):
+		self.program[ self.params[2] ] = self.program[ self.params[0] ] < self.program[ self.params[1] ]
+	
+	#If input is equal to input, set location to 1 or 0
+	def _opcode_8(self):
+		self.program[ self.params[2] ] = self.program[ self.params[0] ] == self.program[ self.params[1] ]
+	
+	#Terminate
+	def _opcode_99(self):
+		self.terminate = True
+
+	##############################################
+
+	#Decodes the current instruction and returns the step distance
+	def _decode_current_instruction(self):
+		self.current_instruction = self.program[self.opcode_pointer]
+		self.param_mode = [0,0,0]
 
 		#Decode parameter instruction
-		val_list = list(str(current_instruction))
+		val_list = list(str(self.current_instruction))
 		instruction_length = len(val_list)
 
 		#Does it specify an immediate mode?
 		if instruction_length > 2:
-			current_instruction = (int(val_list[instruction_length - 2]) * 10) + int(val_list[instruction_length - 1])
+			self.current_instruction = (int(val_list[instruction_length - 2]) * 10) + int(val_list[instruction_length - 1])
 
 			#Set the mode per param
-			for i in range(OP_PARAM_COUNT[current_instruction]):
+			for i in range(self.OP_LIST[self.current_instruction][1]):
 				if i + 3 <= instruction_length:
-					param_mode[i] = int(val_list[(instruction_length - 3) - i])
+					self.param_mode[i] = int(val_list[(instruction_length - 3) - i])
 
-		#Apply correct step distance
-		step_distance = 1 + OP_PARAM_COUNT[current_instruction]
+		new_step_distance = 1 + self.OP_LIST[self.current_instruction][1]
+		return new_step_distance
 
+	def _apply_params(self):
+		self.params = [0,0,0]
+		
 		#Store param values according to mode
-		for i in range(0, OP_PARAM_COUNT[current_instruction]):
+		for i in range(0, self.OP_LIST[self.current_instruction][1]):
 			pointer_offset = i + 1
 
-			if param_mode[i] == 1:
-				params[i] = opcode_pointer + pointer_offset
+			if self.param_mode[i] == 1:
+				self.params[i] = self.opcode_pointer + pointer_offset
 			else:
-				params[i] = opcode_array[opcode_pointer + pointer_offset]
+				self.params[i] = self.program[self.opcode_pointer + pointer_offset]
+
+	def _update(self):
+
+		if self.awaiting_input:
+			if len(self.input_buffer) == 0:
+				return
+
+		step_distance = self._decode_current_instruction()
+
+		self._apply_params()
 
 		#Handle which op it is
-		if current_instruction in OP_PARAM_COUNT:
-			if current_instruction == 1:
-				opcode_array[ params[2] ] = opcode_1( opcode_array[ params[0] ], opcode_array[ params[1] ] )
-
-			elif current_instruction == 2:
-				opcode_array[ params[2] ] = opcode_2( opcode_array [params[0] ], opcode_array[ params[1] ] )
-
-			elif current_instruction == 3:
-				input_val = None
-				#Check the input buffer for auto-input
-				if len(input_buffer) > 0:
-					input_val = input_buffer.pop(0)
-
-				#If we require manual input or there is an empty buffer, request user input
-				if input_val == 'USR' or input_val == None:
-					input_val = opcode_3()
-				#else:
-					#print("[INPUT] Automatic Input: "+str(input_val))
-
-				opcode_array[ params[0] ] = input_val
-
-			elif current_instruction == 4:
-				output_val = opcode_array[ params[0] ]
-				opcode_4( output_val )
-				output_buffer.append(output_val)
-
-			elif current_instruction == 5:
-				if opcode_5( opcode_array[ params[0] ] ):
-					opcode_pointer = opcode_array[ params[1] ]
-					continue
-
-			elif current_instruction == 6:
-				if opcode_6( opcode_array[ params[0] ] ):
-					opcode_pointer = opcode_array[ params[1] ]
-					continue
-
-			elif current_instruction == 7:
-				opcode_array[ params[2] ] = opcode_7( opcode_array[ params[0] ], opcode_array[ params[1] ] )
-
-			elif current_instruction == 8:
-				opcode_array[ params[2] ] = opcode_8( opcode_array[ params[0] ], opcode_array[ params[1] ] )
-
-			elif current_instruction == 99:
-				opcode_99()
-				return output_buffer
-
+		if self.current_instruction in self.OP_LIST:
+			self.OP_LIST[self.current_instruction][0]() #calls the op
 		else:
-			print("Invalid instruction: " + str(current_instruction))
-			break
-	
-		opcode_pointer = opcode_pointer + step_distance
+			print("Invalid instruction: " + str(self.current_instruction))
+			return
 
-	return output_buffer
+		#Don't carry on if we are awaiting input.
+		if self.awaiting_input == True:
+			return
+
+		#Skip manual pointer updating if we did a jump
+		if self.do_jump == True:
+			self.do_jump = False
+			return
+
+		self.opcode_pointer = self.opcode_pointer + step_distance
 
 ####################################################################################
 
 program_data = load_program("input.txt")
 
+computer1 = Computer()
 
 highest_output = 0
 highest_amp_setup = None
@@ -148,9 +181,12 @@ for amp_setup in possible_amps:
 	phase_setting = None
 	output = [0]
 	for i in range(5):
-		program_data_copy = list(program_data)
-		phase_setting = [amp_setup[i], output[0]]
-		output = run_opcode_program(program_data_copy, phase_setting)
+		computer1.load_opcode_program(program_data)
+
+		for i in [amp_setup[i], output[0]]:
+			computer1.input(i)
+
+		output = computer1.run_opcode_program()
 	
 	if output[0] > highest_output:
 		highest_output = output[0]
@@ -158,12 +194,3 @@ for amp_setup in possible_amps:
 
 print(str(highest_output))
 print(str(highest_amp_setup))
-
-amp_setup = [9,8,7,6,5]
-phase_setting = None
-output = [0]
-for i in range(5):
-	program_data_copy = list(program_data)
-	phase_setting = [amp_setup[i], output[0]]
-	output = run_opcode_program(program_data_copy, phase_setting)
-
